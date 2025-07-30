419dc9bdf83f20c45cef08de050e4192e3993f54
Felix Caesar <felixcaesar2002@gmail.com>
2025-07-30 15:35:30 +0000

    improved time measurement, code readability

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{io::Write, thread, time::{Duration, Instant}, u128};
-
+use reqwest::blocking::{Response, Client};
 use clap::{command, Parser};
 
 #[derive(Parser, Debug)]
@@ -19,8 +19,10 @@ struct Args {
 }
 
 fn main() {
+    let client = Client::builder().timeout(Duration::from_secs(300)).build().unwrap_or(Client::new());
+
     let args = Args::parse();
-    let mut failed_requests = 0;
+    let mut failed_requests = 0u32;
     let mut response_times: Vec<Duration> = Vec::new();
     let mut wait_time = 0u128;
 
@@ -30,30 +32,15 @@ fn main() {
     wait_time += args.hours.unwrap_or(0) * 60 * 60 * 1000;
 
     for rep in 0..args.rep {
-        if rep > 0 {
-            if wait_time > 1000 {
-                let seconds = (wait_time / 1000) as u64;
-                for i in (0..=seconds).rev() {
-                    print!("\r({}/{}) {} ", rep + 1, args.rep, i);
-                    std::io::stdout().flush().unwrap();
-                    thread::sleep(Duration::from_secs(1));
-                }
-                thread::sleep(Duration::from_millis((wait_time % 1000) as u64));
-            } else if wait_time > 0 {
-                thread::sleep(Duration::from_millis(wait_time as u64));
-            }
-        } else {
-            print!("\r({}/{}) {} ", rep + 1, args.rep, 0);
-            std::io::stdout().flush().unwrap();
-        }
-        
+        delay_countdown(wait_time, rep + 1, args.rep);
+                
         let start = Instant::now();
-        let status = match reqwest::blocking::get(&args.url) {
+        let response = client.get(&args.url).send();
+        let duration = start.elapsed();
+        let status = match response {
             Ok(response) => {
                 let status = response.status().as_u16();
-                if status < 200 || status >= 300 {
-                    println!("\r(Request {}){}: {}",rep + 1, status, response.text().unwrap_or(String::from("Request unsuccessful")));
-                }
+                parse_response(response, status, &mut failed_requests, rep + 1);
                 status
             }
             _ => {
@@ -62,7 +49,6 @@ fn main() {
                 0u16
             },
         };
-        let duration = start.elapsed();
         if status >= 200 && status < 300 {
             response_times.push(duration);
         }
@@ -115,4 +101,26 @@ fn print_result(result: (u128, u128, u128)) -> String{
     }
 
     text
+}
+
+fn parse_response(response: Response, status: u16, failed_requests: &mut u32, step: u128) {
+    if status < 200 || status >= 300 {
+        *failed_requests += 1;
+        println!("\r(Request {}) {}: {}", step, status, response.text().unwrap_or(String::from("Request did not send a response")));
+    }
+}
+
+fn delay_countdown(wait_time: u128, step: u128, total: u128) {
+    let seconds = (wait_time / 1000) as u64;
+    print!("\rRequest {}/{}     ", step, total);
+    std::io::stdout().flush().unwrap();
+    if step == 1 {
+        return;
+    }
+    for i in (1..=seconds).rev() {
+        print!("\rRequest {}/{} in {} s     ", step, total, i);
+        std::io::stdout().flush().unwrap();
+        thread::sleep(Duration::from_secs(1));
+    }
+    thread::sleep(Duration::from_millis((wait_time % 1000) as u64));           
 }
